129314f82a4564e52b5a0a8ae0d7cd74862d5982
so-suke <attoutekimasshu@gmail.com>
2020-10-14 00:32:45 +0900

    やったこと ・機種別の台番号を分別。

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 const request = require("request");
 const cheerio = require("cheerio");
-const machineNumbers = [
-  21,
-  22,
-  23,
-  24,
-  25,
-  26,
-  27,
-  28,
-  101,
-  102,
-  103,
-  104,
-  147,
-  148,
+const machines = [
+  {
+    name: "Pぱちんこ仮面ﾗｲﾀﾞｰ轟音M6",
+    numbers: [21, 22, 23, 24, 25, 26, 27, 28],
+  },
+  {
+    name: "呪いの7日間2",
+    numbers: [41, 42, 43],
+  },
+  {
+    name: "新必殺仕置人TURBO",
+    numbers: [98, 99, 100],
+  },
+  {
+    name: "真･牙狼",
+    numbers: [101, 102, 103, 104],
+  },
+  {
+    name: "PﾓﾓｷｭﾝｿｰﾄﾞMC",
+    numbers: [147, 148],
+  },
 ];
-const titles_arr = [];
+const machineNumbers = machines.reduce((prev, curr) => {
+  return prev.concat(curr.numbers);
+}, []);
 
 // 今日の大当たり回数が0なら・前日の回転数も足したものを表示、対象とする。
 
@@ -52,21 +60,21 @@ const getData = (machineNumber) => {
 
         console.log(`${machineTitle} ${kaitenNumber}回転`);
 
-        let neraimeKaitenNumber = 0;
-        if (21 <= machineNumber && machineNumber <= 28) {
-          neraimeKaitenNumber = 400;
-        }
-        if (101 <= machineNumber && machineNumber <= 104) {
-          neraimeKaitenNumber = 350;
-        }
-        if (147 <= machineNumber && machineNumber <= 148) {
-          neraimeKaitenNumber = 200;
-        }
-        if (kaitenNumber > neraimeKaitenNumber) {
-          console.log(
-            `＊${machineTitle}の${machineNumber}番台は${kaitenNumber}回転のため打てます。＊`
-          );
-        }
+        // let neraimeKaitenNumber = 0;
+        // if (21 <= machineNumber && machineNumber <= 28) {
+        //   neraimeKaitenNumber = 400;
+        // }
+        // if (101 <= machineNumber && machineNumber <= 104) {
+        //   neraimeKaitenNumber = 350;
+        // }
+        // if (147 <= machineNumber && machineNumber <= 148) {
+        //   neraimeKaitenNumber = 200;
+        // }
+        // if (kaitenNumber > neraimeKaitenNumber) {
+        //   console.log(
+        //     `＊${machineTitle}の${machineNumber}番台は${kaitenNumber}回転のため打てます。＊`
+        //   );
+        // }
         resolve();
       } catch (e) {
         console.error(e);
