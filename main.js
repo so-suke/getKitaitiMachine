d68694ed4bea43e4c027bf1ced26ba947ec459d7
so-suke <attoutekimasshu@gmail.com>
2020-12-10 00:01:56 +0900

    all commit

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const request = require("request");
 const cheerio = require("cheerio");
+
 const machines = [
   {
     name: "Pぱちんこ仮面ﾗｲﾀﾞｰ轟音M6",
-    numbers: [21, 22, 23, 24, 25, 26, 27, 28],
+    numbers: [21, 22, 23, 24, 25],
   },
   {
     name: "呪いの7日間2",
     numbers: [41, 42, 43],
+  },
+  {
+    name: "劇場版まどか☆ﾏｷﾞｶ",
+    numbers: [91, 92, 93, 94],
   },
   {
     name: "新必殺仕置人TURBO",
@@ -18,10 +23,35 @@ const machines = [
     numbers: [101, 102, 103, 104],
   },
   {
+    name: "ｴｳﾞｧﾝｹﾞﾘｵﾝｼﾄ､新生決戦",
+    numbers: [105, 106, 107],
+  },
+  {
+    name: "Pﾊｲｽｸｰﾙｵﾌﾞｻﾞﾃﾞｯﾄﾞ2",
+    numbers: [111, 112, 113],
+  },
+  {
+    name: "Pｳﾙﾄﾗｾﾌﾞﾝ超乱舞",
+    numbers: [126, 127, 128, 129, 130],
+  },
+  {
+    name: "ｺﾞﾙｺﾞ",
+    numbers: [141, 142],
+  },
+  {
+    name: "結城友奈は勇者である",
+    numbers: [143, 144],
+  },
+  {
     name: "PﾓﾓｷｭﾝｿｰﾄﾞMC",
     numbers: [147, 148],
   },
+  {
+    name: "ﾄﾞﾗﾑ海物語INｼﾞｬﾊﾟﾝ",
+    numbers: [198],
+  },
 ];
+
 const machineNumbers = machines.reduce((prev, curr) => {
   return prev.concat(curr.numbers);
 }, []);
@@ -40,41 +70,17 @@ const getData = (machineNumber) => {
         const machineTitle = $("#pachinkoTi>strong").text();
         let kaitenNumber = 0;
         const atariNumberToday = parseInt($(".Text-Big25.Text-Red").text()); // 本日の大当たり回数
-        kaitenNumber += parseInt($(".Text-Big19.Text-Green").text()); // 本日の回転数を足す
+        const $textGreens = $(".Text-Green");
+        kaitenNumber += parseInt($($textGreens["0"]).text()); // 本日の回転数を足す
 
         if (atariNumberToday === 0) {
-          const lastKaitenNumberYesterday = parseInt(
-            $("span:contains('前日最終スタート')")
-              .parent()
-              .next()
-              .text()
-              .slice(0, -6)
-            // $("span:contains('前日最終スタート')")
-            //   .parent()
-            //   .next()
-            //   .text()
-            //   .slice(0, -7)
-          );
+          // const lastKaitenNumberYesterday = parseInt($("span:contains('前日最終スタート')").parent().next().text().slice(0, -7));
+          const lastKaitenNumberYesterday = parseInt($($(".Text-Green")["1"]).text());
+          // console.log(lastKaitenNumberYesterday);
           kaitenNumber += lastKaitenNumberYesterday;
         }
 
         console.log(`${machineTitle} ${kaitenNumber}回転`);
-
-        // let neraimeKaitenNumber = 0;
-        // if (21 <= machineNumber && machineNumber <= 28) {
-        //   neraimeKaitenNumber = 400;
-        // }
-        // if (101 <= machineNumber && machineNumber <= 104) {
-        //   neraimeKaitenNumber = 350;
-        // }
-        // if (147 <= machineNumber && machineNumber <= 148) {
-        //   neraimeKaitenNumber = 200;
-        // }
-        // if (kaitenNumber > neraimeKaitenNumber) {
-        //   console.log(
-        //     `＊${machineTitle}の${machineNumber}番台は${kaitenNumber}回転のため打てます。＊`
-        //   );
-        // }
         resolve();
       } catch (e) {
         console.error(e);
